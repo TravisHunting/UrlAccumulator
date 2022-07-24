c046d39d3a198e04a7df8bb626a1f6cffed63e98
Travis <travis.hunting@gmail.com>
2022-07-24 15:40:47 +1200

    Functionality

--- hyperlinks.py
+++ hyperlinks.py
@@ -4,25 +4,94 @@ import argparse
 import requests
 import sys
 from bs4 import BeautifulSoup as bs
+from publicsuffixlist import PublicSuffixList
+from collections import deque
+import json
 
 
-# def scrapeImageLinksFromIssue(url, lowres):
-#     req = requests.get(url, headers)
-#     soup = bs(req.content, 'html.parser')
-#     soup = soup.prettify()
-#     lines = soup.split("\n")
-#     imageLinks = []
 
-#     for line in lines:
-#         if "https://2.bp.blogspot.com" in line:
-#             imageUrl = extractImageUrlFromText(line, lowres)
-#             imageLinks.append(imageUrl)
+# a function that extracts hyperlinks using requests from a given url
+def scrapeHyperlinksFromURL(url):
+    # get the html from the url
+    html = requests.get(url).text
+    # create a BeautifulSoup object from the html
+    soup = bs(html, 'html.parser')
+    # find all the hyperlinks in the html
+    hyperlinks = soup.find_all('a')
+    # create a list of hyperlinks
+    hyperlinkList = []
+    # iterate through the hyperlinks
+    for link in hyperlinks:
+        # if the link is not empty
+        if link.get('href') != None:
+            # add the link to the list
+            cleanedLink = link.get('href')
+            if cleanedLink[0:2] == "//":
+                cleanedLink = "http:" + cleanedLink
+            elif cleanedLink[0:1] == "/":
+                if url[-1] == "/":
+                    cleanedLink = url + cleanedLink[1:]
+                else:
+                    cleanedLink = url + cleanedLink
+            hyperlinkList.append(cleanedLink)
+    # return the list of hyperlinks
+    return hyperlinkList
 
-#         if checkForCaptcha(line):
-#             solveCaptcha(url)
-#             return scrapeImageLinksFromIssue(url, lowres)
 
-#     return imageLinks
+def runScrape(startUrl, limit):
+    count = 0
+    # url = startUrl
+    hyperlinks = deque()
+    hyperlinks.append(startUrl)
+
+    # print(hyperlinks)
+
+    visitedlinks = set()
+
+    resultDict = {}
+
+    while count < limit:
+        link = hyperlinks[0]
+        if link[0:2] == "//":
+            link = "http:" + link
+        # Don't scrape the same URL twice
+        if link not in visitedlinks:
+            visitedlinks.add(link)
+            
+            print("Scraping: " + link)
+            pagelinks = scrapeHyperlinksFromURL(link)
+            
+            if link not in resultDict:
+                resultDict[link] = {"outgoing": pagelinks}
+            elif "outgoing" not in resultDict[link]:
+                resultDict[link]["outgoing"] = pagelinks
+
+            # Add the new links to the queue
+            hyperlinks.extend(pagelinks)
+
+            # Track total number of scrapes
+            count += 1
+            print("Scraped " + str(count) + " pages")
+
+        # Remove the processed URL
+        hyperlinks.popleft()
+
+        for outgoinglink in pagelinks:
+            if outgoinglink not in resultDict:
+                resultDict[outgoinglink] = {"incoming": [link]}
+            elif "incoming" not in resultDict[outgoinglink]:
+                resultDict[outgoinglink]["incoming"] = [link]
+            else:
+                resultDict[outgoinglink]["incoming"].append(link)
+
+        # If the queue is empty, we're done
+        if len(hyperlinks) == 0:
+            break
+    
+    with open("links.json", "w") as write_file:
+        json.dump(resultDict, write_file, indent=4)
+    
+    return resultDict
 
 
 if __name__ == "__main__":
@@ -34,8 +103,8 @@ if __name__ == "__main__":
                     --limit URLs will have been traversed.', 
                     epilog='Example: hyperlinks.py https://docs.python.org/')
 
-    parser.add_argument('--url', '--url', help='Starting URL')
-    parser.add_argument('--limit', '--limit', help='Number of URLs to traverse', type=int, default=1000, action='store')
+    parser.add_argument('--url', help='Starting URL')
+    parser.add_argument('--limit', help='Number of URLs to traverse', type=int, default=1000, action='store')
 
     # ensure that no args is a help call
     if len(sys.argv)==1:
@@ -44,67 +113,27 @@ if __name__ == "__main__":
 
     arguments = parser.parse_args()
 
-    print("Starting URL: " + arguments.url)
+    # catch urls without https://
+    url = arguments.url
+    if not arguments.url.startswith('https://') and not arguments.url.startswith('http://'):
+        print('No schema detected, attempting to use http://')
+        url = 'http://' + arguments.url
+    # use publicsuffixlist to get domain from url
+    # url = arguments.url.split('//')[1].split('/')[0]
+    # domain = PublicSuffixList().privatesuffix(url)
+
+    # regex to select domain from url
+    # domain = arguments.url.split('//')[1].split('/')[0]
+
+    # 
+
+    # print("Domain: ", domain)
+
+
+    
+
+    print("Starting URL: " + url)
     print("Limit: " + str(arguments.limit))
 
 
-    # if arguments.selenium == True and arguments.selenium_display == True:
-    #     print("Please provide only -s or -sd, not both")
-    #     print("Quitting")
-    #     quit()
-
-    # # set variables from arguments
-    # startURL = arguments.URL
-    # singleIssue = False
-    # if "?id=" in startURL:
-    #     singleIssue = True
-    #     print("You have provided the link for a single issue.")
-    #     print("When providing the URL for the info page, this script will download all issues in the series.")
-
-    # if arguments.folder != None:
-    #     print(f"Title detected, using {arguments.folder} as title")
-    #     comicTitle = arguments.folder
-    # else:
-    #     print("No title specified. Reading title from Url...")
-    #     comicTitle = getComicTitle(startURL, singleIssue)
-    #     print(f"Using title: {comicTitle}")
-
-    # downloadFull = False
-    # if arguments.complete == True and singleIssue == False:
-    #     print("Argument -c detected and comic info URL supplied. Downloading entire comic into one folder")
-    #     downloadFull = True
-
-    # lowres = False
-    # if arguments.lowres == True:
-    #     print("Argument -l detected. Downloading low resolution images")
-    #     lowres = True
-    # else:
-    #     print("Downloading max quality images")
-
-    # useSelenium = False
-    # if arguments.selenium == True:
-    #     print("Argument -s detected. Using Selenium to scrape the page(s)")
-    #     useSelenium = True
-
-    # seleniumDisplay = False
-    # if arguments.selenium_display == True:
-    #     print("Argument -sd detected. Using Selenium in display mode")
-    #     useSelenium = True
-    #     seleniumDisplay = True
-
-    # disableWait = False
-    # if arguments.disable_wait == True:
-    #     print("Argument -d detected. Disabling wait between requests")
-    #     print("This may cause CAPTCHAs to appear more often.")
-    #     disableWait = True
-
-    # issueStart = 0
-    # if arguments.issue == True:
-    #     print("Argument -i detected.")
-    #     print("This is an experimental feature. Use at your own risk. Meant to be used when the script has crashed partway through a series.")
-    #     issueStart = input("What issue would you like to start with? enter an integer: ")
-
-    # print(f"Starting to scrape {comicTitle} from {startURL}")
-
-    # main(downloadFull, singleIssue, comicTitle, lowres, disableWait, startURL, useSelenium, seleniumDisplay, issueStart)
-    # print("\nComic Downloaded")
+    runScrape(url, arguments.limit)
